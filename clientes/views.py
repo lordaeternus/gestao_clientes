a650d469b6d003cded872dabaf83388c8606b086
lordaeternus <32939264+lordaeternus@users.noreply.github.com>
2018-11-04 16:15:55 -0200

    hoje

--- clientes/views.py
+++ clientes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Person
 from .forms import PersonForm
-
+from django.views.generic.list import ListView
 
 @login_required
 def persons_list(request):
@@ -31,7 +31,6 @@ def persons_update(request, id):
 
     return render(request, 'person_form.html', {'form': form})
 
-
 @login_required
 def persons_delete(request, id):
     person = get_object_or_404(Person, pk=id)
@@ -41,3 +40,6 @@ def persons_delete(request, id):
         return redirect('person_list')
 
     return render(request, 'person_delete_confirm.html', {'person': person})
+
+class PersonList(ListView):
+    model = Person
--- home/views.py
+++ home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
+def calcular(v1, v2):
+    return v1 + v2
+
 # TODO: Teste de todo
 def home(request):
-    a = 1
-    b  = 2
-    c = a + b
-    return render(request, 'home.html', {'c':c})
+    return render(request, 'home.html')
 
 # FIXME: Teste de fixme
 def my_logout(request):
