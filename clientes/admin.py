7066c9065d9ff6220014b9f4cf508950e47bd1da
lordaeternus <32939264+lordaeternus@users.noreply.github.com>
2018-11-04 21:13:53 -0200

    Teste

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
+class PersonAdmin(admin.ModelAdmin):
+    fields = ( 'doc', 'first_name', 'last_name', 'age', 'salary', 'bio',
+              'photo' )
+    list_display = ( 'doc', 'first_name', 'last_name', 'age', 'salary', 'bio',
+              'photo' )
 
-admin.site.register(Person)
+    pass
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
